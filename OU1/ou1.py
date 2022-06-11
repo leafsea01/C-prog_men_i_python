5f080ede58679d4e89cf4a181ea6abc0d3942af9
Isak <isaceriksson01@gmail.com>
2022-06-11 18:00:21 +0200

    adding a diretery and a file recrating ou1

---
+++ OU1/ou1.py
@@ -0,0 +1,9 @@
+from pydoc import doc
+
+
+user_input = 0
+while user_input != "-1":
+    print("Press 1 for value change \nPress 2 for adding merchendise \nPress 3 to summerize the purches")
+    user_input = input("Eneter number (-1 for exit): ")
+    print(user_input)
+    
